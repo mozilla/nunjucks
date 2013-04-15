77d6f5e4e700e2b21f7873bad2f4de7a52763c1a
Atul Varma <varmaa@gmail.com>
2013-04-15 18:10:56 -0400

    Added a lib.inArray() helper for IE8 support.
    IE8 doesn't have Array.prototype.indexOf(), so we will add a lib.inArray()
    (the name was taken from jQuery), since we don't want to unexpectedly
    change the prototype of the page including Nunjucks.

--- src/lib.js
+++ src/lib.js
@@ -116,6 +116,41 @@ exports.toArray = function(obj) {
     return Array.prototype.slice.call(obj);
 };
 
+// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/indexOf
+var arrayPrototypeIndexOf = function (searchElement /*, fromIndex */ ) {
+    if (this == null) {
+        throw new TypeError();
+    }
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (len === 0) {
+        return -1;
+    }
+    var n = 0;
+    if (arguments.length > 1) {
+        n = Number(arguments[1]);
+        if (n != n) { // shortcut for verifying if it's NaN
+            n = 0;
+        } else if (n != 0 && n != Infinity && n != -Infinity) {
+            n = (n > 0 || -1) * Math.floor(Math.abs(n));
+        }
+    }
+    if (n >= len) {
+        return -1;
+    }
+    var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+    for (; k < len; k++) {
+        if (k in t && t[k] === searchElement) {
+            return k;
+        }
+    }
+    return -1;
+};
+
+exports.inArray = function(array, searchElement, fromIndex) {
+    return arrayPrototypeIndexOf.call(array, searchElement, fromIndex);
+};
+
 exports.without = function(array) {
     var result = [];
     if (!array) {
