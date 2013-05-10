6c6017d259324b977129c45ec25a842b1c9ff0d1
Atul Varma <varmaa@gmail.com>
2013-05-10 13:14:11 -0400

    added brackets around if statement.

--- src/web-loaders.js
+++ src/web-loaders.js
@@ -3,9 +3,10 @@ var Object = require('./object');
 
 var HttpLoader = Object.extend({
     init: function(baseURL, neverUpdate) {
-        if (typeof(console) !== "undefined" && console.log)
+        if (typeof(console) !== "undefined" && console.log) {
           console.log("[nunjucks] Warning: only use HttpLoader in " +
                       "development. Otherwise precompile your templates.");
+        }
         this.baseURL = baseURL || '';
         this.neverUpdate = neverUpdate;
     },
