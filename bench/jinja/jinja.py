a955c474f135f38cb19f3d2b37514f13f950bf43
James Long <longster@gmail.com>
2013-07-19 17:34:26 -0400

    add jinja test/benchmark

--- bench/jinja/jinja.py
+++ bench/jinja/jinja.py
@@ -3,7 +3,9 @@ import time
 from jinja2 import Template, Environment, FileSystemLoader
 
 env = Environment(loader=FileSystemLoader('.'))
-src = open('index.html').read()
+print env.get_template('index.html').render()
+
+# src = open('index.html').read()
 
 # print(env._generate(env._parse(src, 'poop', 'hello.html'),
 #                     'poop',
@@ -11,18 +13,18 @@ src = open('index.html').read()
 
 # print([x for x in env._tokenize(src, 'poop', 'hello.html')])
 
-env = Environment(loader=FileSystemLoader('.'))
-times = []
-arr = [5]*1000
+# env = Environment(loader=FileSystemLoader('.'))
+# times = []
+# arr = [5]*1000
 
-for i in range(100):
-    env = Environment(loader=FileSystemLoader('.'))
-    t1 = time.time()
-    tmpl = env.get_template('index.html')
-    tmpl.render({'username': 'james',
-                 'arr': arr})
-    t2 = time.time()
+# for i in range(100):
+#     env = Environment(loader=FileSystemLoader('.'))
+#     t1 = time.time()
+#     tmpl = env.get_template('index.html')
+#     tmpl.render({'username': 'james',
+#                  'arr': arr})
+#     t2 = time.time()
 
-    times.append(t2-t1)
+#     times.append(t2-t1)
 
-print( reduce(lambda x, y: x+y, times) / len(times))
+# print( reduce(lambda x, y: x+y, times) / len(times))
