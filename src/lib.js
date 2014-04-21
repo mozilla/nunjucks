e82632b46bc104772192482e4663fcf4bfc9b897
James Long <longster@gmail.com>
2014-04-21 17:41:07 -0400

    small optimizing in html escape function

--- src/lib.js
+++ src/lib.js
@@ -9,6 +9,8 @@ var escapeMap = {
     ">": '&gt;'
 };
 
+var escapeRegex = /[&"'<>]/g;
+
 var lookupEscape = function(ch) {
     return escapeMap[ch];
 };
@@ -84,7 +86,7 @@ exports.TemplateError = function(message, lineno, colno) {
 exports.TemplateError.prototype = Error.prototype;
 
 exports.escape = function(val) {
-    return val.replace(/[&"'<>]/g, lookupEscape);
+  return val.replace(escapeRegex, lookupEscape);
 };
 
 exports.isFunction = function(obj) {
