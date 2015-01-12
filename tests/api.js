40d4ce7135dc42be83865052216a855d40829b15
Samy Pessé <samypesse@gmail.com>
2015-01-12 18:04:29 +0100

    Add tests for relative paths

--- tests/api.js
+++ tests/api.js
@@ -22,5 +22,15 @@
             var child = env.getTemplate('base-inherit.html');
             expect(child.render()).to.be('Foo*Bar*BazFizzle');
         });
+
+        it('should handle correctly relative paths', function() {
+            var env = new Environment(new Loader(templatesPath));
+
+            var child1 = env.getTemplate('relative/test1.html');
+            var child2 = env.getTemplate('relative/test2.html');
+
+            expect(child1.render()).to.be('FooTest1BazFizzle');
+            expect(child2.render()).to.be('FooTest2BazFizzle');
+        });
     });
 })();
