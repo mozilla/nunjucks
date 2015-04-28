b556d4fece0fb7adea2b37eef6ff305ee6eeff5b
James Long <longster@gmail.com>
2015-04-27 22:29:57 -0400

    add wrapper CLI option for loading a precompile plugin (credit to ulion for some of this code)

--- src/node-loaders.js
+++ src/node-loaders.js
@@ -79,7 +79,24 @@ var FileSystemLoader = Loader.extend({
     }
 });
 
+var PrecompiledLoader = Loader.extend({
+    init: function(compiledTemplates) {
+        this.precompiled = compiledTemplates || {};
+    },
+
+    getSource: function(name) {
+        if (this.precompiled[name]) {
+            return {
+                src: { type: "code",
+                       obj: this.precompiled[name] },
+                path: name
+            };
+        }
+        return null;
+    }
+});
 
 module.exports = {
-    FileSystemLoader: FileSystemLoader
+    FileSystemLoader: FileSystemLoader,
+    PrecompiledLoader: PrecompiledLoader
 };
