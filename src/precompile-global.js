1191d8c155126ae1b1fcc798154d2da298aec403
Rafael Hengles <rhengles@gmail.com>
2015-05-07 15:03:40 -0300

    Refactor

--- src/precompile-global.js
+++ src/precompile-global.js
@@ -1,19 +1,23 @@
 'use strict';
 
-function precompileGlobal(name, template, opts) {
+function precompileGlobal(templates, opts) {
+    var out = '', name, template;
     opts = opts || {};
 
-    name = JSON.stringify(name);
+    for ( var i = 0; i < templates.length; i++ ) {
+        name = JSON.stringify(templates[i].name);
+        template = templates[i].template;
 
-    var out = '(function() {' +
-        '(window.nunjucksPrecompiled = window.nunjucksPrecompiled || {})' +
-        '[' + name + '] = (function() {\n' + template + '\n})();\n';
+        out += '(function() {' +
+            '(window.nunjucksPrecompiled = window.nunjucksPrecompiled || {})' +
+            '[' + name + '] = (function() {\n' + template + '\n})();\n';
 
-    if(opts.asFunction) {
-        out += 'return function(ctx, cb) { return nunjucks.render(' + name + ', ctx, cb); }\n';
+        if(opts.asFunction) {
+            out += 'return function(ctx, cb) { return nunjucks.render(' + name + ', ctx, cb); }\n';
+        }
+
+        out += '})();\n';
     }
-
-    out += '})();\n';
     return out;
 }
 
