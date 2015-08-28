259485093f17a337ca34fc8a7962eca48a26fcc7
James Long <longster@gmail.com>
2015-08-27 21:14:39 -0400

    use chokidar correctly (fixes #450)

--- src/node-loaders.js
+++ src/node-loaders.js
@@ -34,19 +34,15 @@ var FileSystemLoader = Loader.extend({
         }
 
         if(opts.watch) {
-            var _this = this;
             // Watch all the templates in the paths and fire an event when
             // they change
-            lib.each(_this.searchPaths, function(p) {
-                if(existsSync(p)) {
-                    var watcher = chokidar.watch(p);
-
-                    watcher.on('all', function(event, fullname) {
-                        fullname = path.resolve(fullname);
-                        if(event === 'change' && fullname in _this.pathsToNames) {
-                            _this.emit('update', _this.pathsToNames[fullname]);
-                        }
-                    });
+            var searchPaths = this.searchPaths.filter(function(p) { return existsSync(p); });
+            var watcher = chokidar.watch(searchPaths);
+            var _this = this;
+            watcher.on('all', function(event, fullname) {
+                fullname = path.resolve(fullname);
+                if(event === 'change' && fullname in _this.pathsToNames) {
+                    _this.emit('update', _this.pathsToNames[fullname]);
                 }
             });
         }
