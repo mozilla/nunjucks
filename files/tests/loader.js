922764a1d6708ad325abe18c4b0a5515220a695e
Carl Meyer <carl@oddbird.net>
2015-09-21 14:44:30 -0600

    Update docs for v2.1.0.

--- files/tests/loader.js
+++ files/tests/loader.js
@@ -1,21 +1,34 @@
 (function() {
     'use strict';
 
-    var expect, Environment, Loader, templatesPath;
+    var expect, Environment, WebLoader, FileSystemLoader, templatesPath;
 
     if(typeof require !== 'undefined') {
         expect = require('expect.js');
         Environment = require('../src/environment').Environment;
+        WebLoader = require('../src/web-loaders').WebLoader;
+        FileSystemLoader = require('../src/node-loaders').FileSystemLoader;
         templatesPath = 'tests/templates';
     }
     else {
         expect = window.expect;
         Environment = nunjucks.Environment;
-        Loader = nunjucks.WebLoader;
+        WebLoader = nunjucks.WebLoader;
+        FileSystemLoader = nunjucks.FileSystemLoader;
         templatesPath = '../templates';
     }
 
     describe('loader', function() {
+        it('should have default opts', function() {
+            var webLoader = new WebLoader(templatesPath);
+            var fileSystemLoader = new FileSystemLoader(templatesPath);
+            expect(webLoader).to.be.a(WebLoader);
+            expect(webLoader.useCache).to.be(false);
+            expect(webLoader.async).to.be(false);
+            expect(fileSystemLoader).to.be.a(FileSystemLoader);
+            expect(fileSystemLoader.noCache).to.be(false);
+        });
+
         it('should allow a simple loader to be created', function() {
             // From Docs: http://mozilla.github.io/nunjucks/api.html#writing-a-loader
             // We should be able to create a loader that only exposes getSource
