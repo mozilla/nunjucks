db18376feb27d26aaf6131abc2c4061be538e2f8
Ivan Kleshnin <ivan@paqmind.com>
2015-12-20 12:32:06 +0200

    Add optional "preserve linebreaks" behavior for `striptags` filter
    It's often desired to pipe {{ text | striptags | nl2br }}.
    For example, when we're rendering email template from feedback form.
    This commit adds an option to remove tags, keeping significant whitespace.

--- src/filters.js
+++ src/filters.js
@@ -398,10 +398,21 @@ var filters = {
         return r.copySafeness(obj, obj);
     },
 
-    striptags: function(input) {
+    striptags: function(input, preserve_linebreaks) {
         input = normalize(input, '');
+        preserve_linebreaks = preserve_linebreaks || false;
         var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>|<!--[\s\S]*?-->/gi;
-        return r.copySafeness(input, filters.trim(input.replace(tags, '')).replace(/\s+/gi, ' '));
+        var res = '';
+        if (preserve_linebreaks) {
+            res = filters.trim(input.replace(tags, ''))
+                .replace(/^ +| +$/gm, '')     // remove leading and trailing spaces
+                .replace(/ +/g, ' ')          // squash consequent spaces
+                .replace(/(\r\n)/g, '\n')     // normalize linebreaks (CRLF -> LF)
+                .replace(/\n\n\n+/g, '\n\n'); // squash abnormal consequent linebreaks
+        } else {
+            res = filters.trim(input.replace(tags, '')).replace(/\s+/gi, ' ');
+        }
+        return r.copySafeness(input, res);
     },
 
     title: function(str) {
