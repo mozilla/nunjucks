77813b7e45ffa19246a9793f8dbf1e0ca9423f4b
Alexander Inozemtsev <alexander.inozemtsev@gmail.com>
2016-09-14 12:15:12 +0300

    Update docs for 2.5.2 release.

--- files/nunjucks-slim.js
+++ files/nunjucks-slim.js
@@ -1,4 +1,4 @@
-/*! Browser bundle of nunjucks 2.5.1 (slim, only works with precompiled templates) */
+/*! Browser bundle of nunjucks 2.5.2 (slim, only works with precompiled templates) */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
@@ -1549,8 +1549,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	    },
 
 	    safe: function(str) {
+	        if (str instanceof r.SafeString) {
+	            return str;
+	        }
 	        str = (str === null || str === undefined) ? '' : str;
-	        return r.markSafe(str);
+	        return r.markSafe(str.toString());
 	    },
 
 	    first: function(arr) {
