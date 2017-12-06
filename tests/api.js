a6be16d4a9731f076cb8b6b5b1dc3cf2e4ab36e1
Frankie Dintino <fdintino@gmail.com>
2017-12-06 16:08:54 -0500

    Minor tweaks so that mocha browser tests pass again

--- tests/api.js
+++ tests/api.js
@@ -1,8 +1,7 @@
 (function() {
     'use strict';
 
-    var expect, util, Environment, Loader, templatesPath;
-    var path = require('path');
+    var expect, util, Environment, Loader, templatesPath, path;
 
     if(typeof require !== 'undefined') {
         expect = require('expect.js');
@@ -10,6 +9,7 @@
         Environment = require('../src/environment').Environment;
         Loader = require('../src/node-loaders').FileSystemLoader;
         templatesPath = 'tests/templates';
+        path = require('path');
     }
     else {
         expect = window.expect;
@@ -26,24 +26,6 @@
             expect(child.render()).to.be('Foo*Bar*BazFizzle');
         });
 
-        it('should handle correctly relative paths', function() {
-            var env = new Environment(new Loader(templatesPath));
-
-            var child1 = env.getTemplate('relative/test1.njk');
-            var child2 = env.getTemplate('relative/test2.njk');
-
-            expect(child1.render()).to.be('FooTest1BazFizzle');
-            expect(child2.render()).to.be('FooTest2BazFizzle');
-        });
-
-        it('should handle correctly cache for relative paths', function() {
-            var env = new Environment(new Loader(templatesPath));
-
-            var test = env.getTemplate('relative/test-cache.njk');
-
-            expect(util.normEOL(test.render())).to.be('Test1\nTest2');
-        });
-
         it('should only call the callback once when conditional import fails', function(done) {
             var env = new Environment(new Loader(templatesPath));
             var called = 0;
@@ -55,11 +37,31 @@
             setTimeout(done, 0);
         });
 
-        it('should handle correctly relative paths in renderString', function() {
-            var env = new Environment(new Loader(templatesPath));
-            expect(env.renderString('{% extends "./relative/test1.njk" %}{% block block1 %}Test3{% endblock %}', {}, {
-                path: path.resolve(templatesPath, 'string.njk')
-            })).to.be('FooTest3BazFizzle');
-        });
+        if (typeof path !== 'undefined') {
+            it('should handle correctly relative paths', function() {
+                var env = new Environment(new Loader(templatesPath));
+
+                var child1 = env.getTemplate('relative/test1.njk');
+                var child2 = env.getTemplate('relative/test2.njk');
+
+                expect(child1.render()).to.be('FooTest1BazFizzle');
+                expect(child2.render()).to.be('FooTest2BazFizzle');
+            });
+
+            it('should handle correctly cache for relative paths', function() {
+                var env = new Environment(new Loader(templatesPath));
+
+                var test = env.getTemplate('relative/test-cache.njk');
+
+                expect(util.normEOL(test.render())).to.be('Test1\nTest2');
+            });
+
+            it('should handle correctly relative paths in renderString', function() {
+                var env = new Environment(new Loader(templatesPath));
+                expect(env.renderString('{% extends "./relative/test1.njk" %}{% block block1 %}Test3{% endblock %}', {}, {
+                    path: path.resolve(templatesPath, 'string.njk')
+                })).to.be('FooTest3BazFizzle');
+            });
+        }
     });
 })();
