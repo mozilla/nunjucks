2006f6f451a41d63707a2d86cd9918ec8b98a168
Alexey <lesha.ogonkov@gmail.com>
2020-06-13 16:42:33 +0300

    Add missing blank line

--- nunjucks/src/environment.js
+++ nunjucks/src/environment.js
@@ -11,6 +11,7 @@ import * as compiler from './compiler';
 import * as lib from './lib';
 
 const {handleError, Frame} = globalRuntime;
+
 // If the user is using the async API, *always* call it
 // asynchronously even if the template was synchronous.
 function callbackAsap(cb, err, res) {
